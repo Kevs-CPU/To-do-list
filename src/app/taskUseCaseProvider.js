841774623608e5462cdd1d72f7e4679a2fc576f7
John Kevin <jameo@example.com>
2026-07-03 09:26:08 +0800

    Refactor and format todo list code

--- src/app/taskUseCaseProvider.js
+++ src/app/taskUseCaseProvider.js
@@ -1,14 +1,14 @@
 import { getTaskRepository } from "./taskRepositoryProvider";
-import { add_task_UseCase } from "../domain/usecases/add_task_usecase";
-import { remove_task_usecase } from "../domain/usecases/remove_task_usecase";
-import { update_task_usecase } from "../domain/usecases/update_task_usecase";
-import { get_all_tasks_usecase } from "../domain/usecases/get_all_tasks_usecase";
-import { get_task_usecase } from "../domain/usecases/get_task_usecase";
+import { AddTaskUseCase } from "../domain/usecases/add_task_usecase";
+import { RemoveTaskUseCase } from "../domain/usecases/remove_task_usecase";
+import { UpdateTaskUseCase } from "../domain/usecases/update_task_usecase";
+import { GetAllTaskUseCase } from "../domain/usecases/get_all_tasks_usecase";
+import { GetTaskUseCase } from "../domain/usecases/get_task_usecase";
 
 const repo = getTaskRepository();
 
-export const addTaskUseCase = new add_task_UseCase(repo);
-export const removeTaskUseCase = new remove_task_usecase(repo);
-export const updateTaskUseCase = new update_task_usecase(repo);
-export const getAllTasksUseCase = new get_all_tasks_usecase(repo);
-export const getTaskUseCase = new get_task_usecase(repo);
+export const addTaskUseCase = new AddTaskUseCase(repo);
+export const removeTaskUseCase = new RemoveTaskUseCase(repo);
+export const updateTaskUseCase = new UpdateTaskUseCase(repo);
+export const getAllTasksUseCase = new GetAllTaskUseCase(repo);
+export const getTaskUseCase = new GetTaskUseCase(repo);
