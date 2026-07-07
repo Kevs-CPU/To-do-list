5549e5253f9cd770659e56cf0ba7d9941f0acfbf
John Kevin <jameo@example.com>
2026-07-07 10:00:17 +0800

    Refactor

--- src/redux/legacyStore.ts
+++ src/redux/legacyStore.ts
@@ -4,4 +4,14 @@ import { taskReducer } from "./task/task.reducers";
 
 const rootReducer = combineReducers({ tasks: taskReducer });
 
-export const legacyStore = createStore(rootReducer, applyMiddleware(thunk));
+export const legacyStore = createStore(rootReducer, applyMiddleware(thunk));  
+
+
+
+
+
+// Command the original path in your file disk
+// cd "to do-List"    //npm run dev -- --open para mag open siya sa browser 
+// First task Create todo-list front end only using react.js 
+// Second task Apply Redux toollkit using Existing task 
+// therd task Enhance the Existing task to clean architecture  
